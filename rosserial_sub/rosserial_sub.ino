aa023f8bf8bc41eca4f30b926a398e022441372f
sentry5588 <sentry5588@gmx.com>
2020-12-24 22:05:26 -0500

    Finished Ardruino ros package that takes two motor speeds and drive stepper motors via A4988 using PWM

--- rosserial_sub/rosserial_sub.ino
+++ rosserial_sub/rosserial_sub.ino
@@ -1,5 +1,6 @@
 /*
     ROS node to subscribe to motor steps and drive stepper motors
+    Code compiled in Arduino 1.8.13 in Ubuntu 18.04
 */
 #include <PWM.h>
 #include <ros.h>
@@ -8,11 +9,12 @@
 #define HALF_DUTY_CYCLE 127
 #define ZERO_DUTY_CYCLE 0
 #define LEFT_STEP_PIN 2
-#define BUG_EXTRA_LEFT_STEP_PIN 5
+#define BUG_EXTRA_LEFT_STEP_PIN 5 // An Arduino (or IDE) bug that required PIN5 to be written while writing to PIN2
 #define RIGHT_STEP_PIN 11
 #define LEFT_DIR 52
 #define RIGHT_DIR 53
 #define A4988_MS2 51
+#define ON_BOARD_LED_PIN 13
 
 unsigned long previousMillis_ros_spin = 0;
 unsigned long previousMillis_LED = 0;
@@ -25,7 +27,7 @@ ros::NodeHandle nh;
 void left_motor_speed_callback(const std_msgs::Int32& s) {
   int32_t v = (int32_t) s.data;
 
-  if (v < 0) { // forward speed 
+  if (v < 0) { // forward speed
     digitalWrite(LEFT_DIR, HIGH);
     pwmWrite(LEFT_STEP_PIN, HALF_DUTY_CYCLE);
     pwmWrite(BUG_EXTRA_LEFT_STEP_PIN, HALF_DUTY_CYCLE);
@@ -38,13 +40,13 @@ void left_motor_speed_callback(const std_msgs::Int32& s) {
     pwmWrite(BUG_EXTRA_LEFT_STEP_PIN, ZERO_DUTY_CYCLE);
   }
   bool success = SetPinFrequencySafe(LEFT_STEP_PIN, v);
-//  nh.loginfo("L_CB: ");
+  //  nh.loginfo("L_CB: ");
 }
 
 void right_motor_speed_callback(const std_msgs::Int32& s) {
   int32_t v = (int32_t) s.data;
 
-  if (v < 0) { // forward speed 
+  if (v < 0) { // forward speed
     digitalWrite(RIGHT_DIR, HIGH);
     pwmWrite(RIGHT_STEP_PIN, HALF_DUTY_CYCLE);
   } else if (v > 0) { // reverse speed
@@ -54,7 +56,7 @@ void right_motor_speed_callback(const std_msgs::Int32& s) {
     pwmWrite(RIGHT_STEP_PIN, ZERO_DUTY_CYCLE);
   }
   bool success = SetPinFrequencySafe(RIGHT_STEP_PIN, v);
-  nh.loginfo("R_CB: ");
+  //  nh.loginfo("R_CB: ");
 }
 
 ros::Subscriber<std_msgs::Int32> sub_left("left_motor_hz", &left_motor_speed_callback );
@@ -62,21 +64,23 @@ ros::Subscriber<std_msgs::Int32> sub_right("right_motor_hz", &right_motor_speed_
 
 void setup()
 {
-  pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(sub_left);
   nh.subscribe(sub_right);
 
   //initialize all timers except for 0, to save time keeping functions
   InitTimersSafe();
-  pinMode(A4988_MS2, OUTPUT);
+  pinMode(A4988_MS2, OUTPUT); // For 1/4 micro-step operation of A4988
   pinMode(LEFT_DIR, OUTPUT);
   pinMode(RIGHT_DIR, OUTPUT);
+  pinMode(ON_BOARD_LED_PIN, OUTPUT);
 }
 
 void loop()
 {
   unsigned long currentMillis = millis();
+
+  // Use faster loop to drive the motor
   if (currentMillis - previousMillis_ros_spin >= interval_ros_spin) {
     previousMillis_ros_spin = currentMillis;
     nh.spinOnce();
@@ -84,9 +88,11 @@ void loop()
   } else {
     empty_loop_count ++;
   }
+
+  // Use slow loop to drive LED to indicate run status
   if (currentMillis - previousMillis_LED >= interval_LED) {
     previousMillis_LED = currentMillis;
-    digitalWrite(13, HIGH - digitalRead(13)); // blink the on-board led as a status indicator
-    digitalWrite(A4988_MS2, HIGH);
+    digitalWrite(ON_BOARD_LED_PIN, HIGH - digitalRead(ON_BOARD_LED_PIN)); // blink the on-board led as a status indicator
+    digitalWrite(A4988_MS2, HIGH); // Set it in a slow loop to save computation power
   }
 }
